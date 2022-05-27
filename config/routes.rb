1afd036ac71d67eebf2583a7afef741e45487b39
hayato2022 <gaomusunxiang@gmail.com>
2022-05-27 07:46:05 +0000

    [Add]コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get root :to => 'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create]
+    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
   get 'homes/about', as: 'about'
