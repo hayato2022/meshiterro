ac492a7d451be81e4052859d01fa5146c7d8ebd5
hayato2022 <gaomusunxiang@gmail.com>
2022-06-01 03:49:59 +0000

    [Add]header.footerレイアウト

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,12 +2,12 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-const webpack = require("webpack")
+const webpack = require('webpack')
 environment.plugins.prepend(
-   'provide',
-   new webpack.ProvidePlugins({
-     $: 'jquery/src/jquery',
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
-   })
-  )
+  })
+)
